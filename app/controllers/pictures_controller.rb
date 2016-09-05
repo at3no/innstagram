e6607e804a7b29bafcb4ecc39c953c877d19c8aa
山能敦志 <AtsushiYamanou@yamanouatsushi-no-MacBook-Pro.local>
2016-09-05 16:29:24 +0900

    first commit

---
+++ app/controllers/pictures_controller.rb
@@ -0,0 +1,60 @@
+class PicturesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_picture, only: [:edit, :update, :destroy]
+
+  def index
+    @pictures = Picture.all
+  end
+
+  def new
+    if params[:back]
+      @picture = Picture.new(pictures_params)
+    else
+      @picture = Picture.new
+    end
+  end
+
+  def create
+    @picture = Picture.new(pictures_params)
+    @picture.user_id = current_user.id
+    if @picture.save
+      redirect_to pictures_path, notice: "投稿しました！"
+    else
+      render action: 'new'
+    end
+  end
+
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+
+    if @picture.update(pictures_params)
+      redirect_to pictures_path, notice: "ブログを更新しました！"
+    else
+      render action: 'edit'
+    end
+  end
+
+  def destroy
+    @picture = Picture.find(params[:id])
+    @picture.destroy
+    redirect_to pictures_path, notice: "ブログを削除しました！"
+  end
+
+  def confirm
+    @picture = Picture.new(pictures_params)
+    render :new if @picture.invalid?
+  end
+
+  private
+    def pictures_params
+      params.require(:picture).permit(:name, :pic)
+    end
+
+    def set_picture
+      @picture = Picture.find(params[:id])
+    end
+end
---
+++ app/models/picture.rb
@@ -0,0 +1,5 @@
+class Picture < ActiveRecord::Base
+  validates :name, presence: true
+
+  belongs_to :user
+end
